e26b04261a491ba4d2d0ed9cfb249632f0954c44
yuki <marukesu11@icloud.com>
2019-05-08 00:12:28 +0900

    8まで完了

---
+++ Instagram/const.swift
@@ -0,0 +1,13 @@
+//
+//  const.swift
+//  Instagram
+//
+//  Created by 川田有紀 on 2019/05/07.
+//  Copyright © 2019 jp.techacademy.yuki.kawata. All rights reserved.
+//
+
+import Foundation
+
+struct Const{
+    static let PostPath = "posts"
+}
